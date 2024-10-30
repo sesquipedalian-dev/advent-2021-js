c0925c9fdbb33f8c752f28a9aacd5a0045c2b722
sesquipedalian.dev <sesquipedalian.dev@gmail.com>
2024-10-29 22:51:51 -0600

    some stuff

--- README.md
+++ README.md
@@ -3,9 +3,17 @@
 [Advent of Code 2021](https://adventofcode.com/2021) as a vehicle for noodling with server-side javascript. 
 
 # dependencies
+[NPM](https://example.com)
 [Node](https://nodejs.org/en/learn/getting-started/how-to-install-nodejs)
 
 In my case, I'm using [WSL2 and nvm](https://learn.microsoft.com/en-us/windows/dev-environment/javascript/nodejs-on-wsl0).
+
+# running
+```
+npm i
+npm run day1
+```
+
 # structure
 
 - dayX
@@ -16,3 +24,6 @@ In my case, I'm using [WSL2 and nvm](https://learn.microsoft.com/en-us/windows/d
   - aoc.js (scraper / fetch stuff from the aoc website)
 - README.md (this file)
 - LICENSE (This project is [MIT licensed](https://choosealicense.com/licenses/mit/))
+- token.txt (store the cookie value from the advent of code site in this file for use in downloading inputs)
+- package.json (npm)
+- package-lock.json (npm)
--- day1/day1.js
+++ day1/day1.js
@@ -1,1 +1,48 @@
 console.log('hello world!');
+const fs = require('node:fs/promises');
+const axios = require('axios');
+const codeBlocksRegex = /<code>(.*?)<\/code>/mgs;
+
+const axiosInstance = axios.create({
+    withCredentials: true // Ensure credentials are sent with every request
+   });
+// Add a request interceptor to include the cookie in the headers
+axiosInstance.interceptors.request.use(async config => {
+    // Set the cookie in the header
+    config.headers['Cookie'] = await authHeader();
+    return config;
+   }, error => {
+    // Do something with request error
+    return Promise.reject(error);
+   });
+
+let authHeaderMemo;
+const authHeader = async () => {
+    if (authHeaderMemo === undefined) {
+      const token = await fs.readFile('./token.txt', {encoding: 'utf8'})
+        .catch(err => fs.readFile('../token.txt',{encoding: 'utf8'}))
+      authHeaderMemo = `session=${token}`
+    }
+    return authHeaderMemo;
+}
+
+const fetchDayCodes = (year, day) => {
+    const url = `https://adventofcode.com/${year}/day/${day}`;
+    return axiosInstance.get(url, null, {headers: authHeader()}).then(({data}) => data.matchAll(codeBlocksRegex));
+};
+
+const fetchDayInput = (year, day) => { 
+    const url = `https://adventofcode.com/${year}/day/${day}/input`;
+    return axiosInstance.get(url, null,  {headers: authHeader()}).then(({data}) => data)
+}
+
+fetchDayInput('2021', '1').then(y2021d1Input => {
+    // console.log('2021 day 1 input', y2021d1Input);
+});
+
+fetchDayCodes('2021', '1').then(codes => { 
+    // console.log('all the codes', [...codes].map(m => m[1]));
+
+    sample1 = codes.next().value[1].split("\n").map(n => parseInt(n)).filter(n => n > 0);
+    console.log('sample case', sample1);
+})
