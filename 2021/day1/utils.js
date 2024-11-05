1af502424c52e35877e915e1f607c3c6975d16cd
sesquipedalian.dev <sesquipedalian.dev@gmail.com>
2024-11-05 06:47:45 -0700

    more eslint fixes

--- 2021/day1/utils.js
+++ 2021/day1/utils.js
@@ -1,2 +1,10 @@
 const numRegex = /\d+(\.\d+)?/;
-export const stringListToFirstInt = (strings) => strings.map((s) => s.match(numRegex)).filter((m) => m !== null).map((m) => m[0]).map((s) => parseInt(s));
+const stringListToFirstInt = (strings) => (
+  strings.map((s) => (
+    s.match(numRegex).filter((m) => m !== null).map((m) => m[0]).map((s2) => parseInt(s2, 10))
+  ))
+);
+
+export default {
+  stringListToFirstInt,
+};
