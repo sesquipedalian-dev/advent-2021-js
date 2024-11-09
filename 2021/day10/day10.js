a922451eb20b18e74e883407ea7f302e69bcce2c
sesquipedalian.dev <sesquipedalian.dev@gmail.com>
2024-11-09 03:04:52 -0700

    day 10 part 2

--- 2021/day10/day10.js
+++ 2021/day10/day10.js
@@ -5,6 +5,7 @@ import utils from './utils.js';
 const CLOSERS = ']})>'.split('')
 const OPENERS = '[{(<'.split('')
 const POINTS = [57, 1197, 3, 25137]
+const OPENER_POINTS = [2, 3, 1, 4]
 
 const part1 = (lines) => {
     // console.log('part 1', lines)
@@ -32,13 +33,57 @@ const part1 = (lines) => {
     }, 0)
 }
 
-const part2 = () => {
-    return null;
+const part2 = (lines) => {
+    // so part 2, we still need a stack
+    // now throw out / skip corrupted lines instead
+    // for lines that finish with a non-empty stack, we simply pop the stack,
+    // lookup the opener points corresponding to those entries, then convert those digits to base 5
+    // to get the score for that line 
+    // then we sort the lines by score, 
+    // and return the middle score Math.floor(score.length / 2), since it's guaranteed to be an odd number of incomplete lines
+
+    const completionScores = lines.reduce((scores, line, index) => {
+        const remainingOpeners = line.reduce(([done, stack], item) => {
+            if(done) { 
+                return [true, []]
+            }
+
+            if (OPENERS.indexOf(item) >= 0) {
+                return [false, [...stack, item]]
+            }
+
+            const currentOpenerIndex = OPENERS.indexOf(stack[stack.length - 1])
+            const closerIndex = CLOSERS.indexOf(item)
+            if (currentOpenerIndex != closerIndex) { 
+                // we want to skip this line, so bail out with an empty stack
+                return [true, []]
+            }
+
+            // pop the stack
+            return [false, stack.slice(0, stack.length - 1)]
+        }, [false, []])[1]
+        
+        if (remainingOpeners.length > 0) { 
+            let digits = ''
+            while(remainingOpeners.length > 0) { 
+                const opener = remainingOpeners.pop()
+                const index = OPENERS.indexOf(opener)
+                digits += OPENER_POINTS[index]
+            }
+            // console.log('completion score', index, digits, parseInt(digits, 5))
+            return [...scores, parseInt(digits, 5)]
+        }
+
+        return [...scores]
+    }, [])
+
+    completionScores.sort((a,b) => a-b)
+    return completionScores[Math.floor(completionScores.length / 2)]
 }
 
 
 aoc.fetchDayCodes('2021', '10').then(codes => { 
-    // console.log('all the codes', codes.map((c, i) => [c, i]));
+    // console.log('all the codes', codes.slice(50).map((c, i) => [c, i]));
     // return;
 
     const sample1 = codes[20].replaceAll('&lt;', '<').replaceAll('&gt;', '>').split('\n').map(l => l.split('')).filter(l => l.length > 0)
@@ -50,12 +95,12 @@ aoc.fetchDayCodes('2021', '10').then(codes => {
         return;
     }
 
-    // const part2Answer = part2(sample1);
-    // const part2Correct = utils.parseAnswerFromEms(codes[codes.length - 1]);
-    // if (part2Answer != part2Correct) {
-    //     console.log('failed on part 2 test case', part2Answer, part2Correct);
-    //     return;
-    // }
+    const part2Answer = part2(sample1);
+    const part2Correct = utils.parseAnswerFromEms(codes[101]);
+    if (part2Answer != part2Correct) {
+        console.log('failed on part 2 test case', part2Answer, part2Correct);
+        return;
+    }
 
     Promise.all([aoc.fetchDayInput('2021', '10'), aoc.fetchDayAnswers('2021', '10')]).then(([input, answers]) => {
         const list_of_ints = input.replaceAll('&lt;', '<').replaceAll('&gt;', '>').split('\n').map(l => l.split('')).filter(l => l.length > 0)
@@ -66,12 +111,12 @@ aoc.fetchDayCodes('2021', '10').then(codes => {
         }
         console.log('part 1 answer', answer2, answer2Right);
 
-        // const answer3 = part2(list_of_ints);
-        // let answer3Right;
-        // if (answers.length > 1) { 
-        //     answer3Right = answers[1] == answer3.toString();
-        // }
-        // console.log('part 2 answer', answer3, answer3Right);
+        const answer3 = part2(list_of_ints);
+        let answer3Right;
+        if (answers.length > 1) { 
+            answer3Right = answers[1] == answer3.toString();
+        }
+        console.log('part 2 answer', answer3, answer3Right);
     });
 })
 
