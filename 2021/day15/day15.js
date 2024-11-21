ef35d536aba2fa39c7cceae9be33ac96dd61bdf0
sesquipedalian.dev <sesquipedalian.dev@gmail.com>
2024-11-21 14:44:41 -0700

    iteration

--- 2021/day15/day15.js
+++ 2021/day15/day15.js
@@ -1,28 +1,31 @@
 
 import aoc from '../../util/aoc.js';
 import utils from './utils.js';
+import { MinPriorityQueue } from '@datastructures-js/priority-queue';
 
 const part1 = (grid) => {
     // Djkstra's ? 
     const target = [grid.rows - 1, grid.columns - 1]
     let steps = 0
-    while(steps < 10_000 && !grid.at(target[0], target[1]).visited) {
+    // interface IGridEntry {
+    //     row: Number,
+    //     column: Number,
+    //     best: Number
+    // }
+    let unvisited = new MinPriorityQueue(({row, column}) => grid.at(row, column).best)
+    grid.iterate((row, column) => unvisited.enqueue({row, column}))
+
+    while(steps < 1_000_000 && !grid.at(target[0], target[1]).visited) {
         steps += 1
-        let current = {
-            best: Number.MAX_SAFE_INTEGER
-        }
-        grid.iterate((row, column, {best, visited}) => {
-            // console.log('iteration', row, column, best)
-            if (!visited && best < current.best) { 
-                current.best = best
-                current.row = row
-                current.column = column
-            }
-        })
+     
+        const current = unvisited.dequeue()
         // console.log('iteration', current)
         if (current.row == target[0] && current.column == target[1]){
             // reached end
             break
+        }
+        if (steps % 2_000 === 0) {
+            console.log('.', current)
         }
         grid.at(current.row, current.column).visited = true
 
@@ -32,10 +35,12 @@ const part1 = (grid) => {
                 return
             }
             
-            const cost = current.best + grid.at(row, column).risk
-            // console.log('feeling neighborly', row, column, cost)
+            const cost = grid.at(current.row, current.column).best + grid.at(row, column).risk
+            // console.log('feeling neighborly', row, column, cost, grid.at(row, column).best)
             if (cost < grid.at(row, column).best) {
                 grid.at(row, column).best = cost
+                unvisited.remove(({r2, c2}) => r2 == row && c2 == column)
+                unvisited.enqueue({row, column, cost})
             }
         })
     }
@@ -43,12 +48,33 @@ const part1 = (grid) => {
     return grid.at(grid.rows - 1, grid.columns - 1).best
 }
 
-const part2 = () => {
-    return null;
+const part2 = (grid) => {
+    const extendedString = utils.initArray(grid.rows * 5, (row) => utils.initArray(grid.columns * 5, (column) => {
+        const rowExtension = Math.floor(row / grid.rows)
+        const rowIndex = row % grid.rows
+        const columnExtension = Math.floor(column / grid.columns)
+        const columnIndex = column % grid.columns
+        return (((grid.at(rowIndex, columnIndex).risk + rowExtension + columnExtension - 1) % 9) + 1).toString()
+    }).join("")).join("\n")
+    console.log('extended grid', extendedString)
+
+    const extendedGrid = new utils.Grid(
+        extendedString,
+        '',
+        (s, row, column) => ({
+            visited: false,
+            risk: parseInt(s, 10),
+            best: (row == 0 && column == 0) ? 0 : Number.MAX_SAFE_INTEGER
+        })
+    )
+
+    return part1(extendedGrid)
 }
 
 
 aoc.fetchDayCodes('2021', '15').then(codes => { 
+    // console.log(Number.MAX_SAFE_INTEGER)
+    // return
     // console.log('all the codes', codes.map((c, i) => [c, i]));
     // return;
 
@@ -69,12 +95,12 @@ aoc.fetchDayCodes('2021', '15').then(codes => {
         return;
     }
 
-    // const part2Answer = part2(sample1);
-    // const part2Correct = utils.parseAnswerFromEms(codes[11]);
-    // if (part2Answer != part2Correct) {
-    //     console.log('failed on part 2 test case', part2Answer, part2Correct);
-    //     return;
-    // }
+    const part2Answer = part2(sample1);
+    const part2Correct = utils.parseAnswerFromEms(codes[11]);
+    if (part2Answer != part2Correct) {
+        console.log('failed on part 2 test case', part2Answer, part2Correct);
+        return;
+    }
 
     Promise.all([aoc.fetchDayInput('2021', '15'), aoc.fetchDayAnswers('2021', '15')]).then(([input, answers]) => {
         const list_of_ints = new utils.Grid(
@@ -93,12 +119,13 @@ aoc.fetchDayCodes('2021', '15').then(codes => {
         }
         console.log('part 1 answer', answer2, answer2Right);
 
-        // const answer3 = part2(list_of_ints);
-        // let answer3Right;
-        // if (answers.length > 1) { 
-        //     answer3Right = answers[1] == answer3.toString();
-        // }
-        // console.log('part 2 answer', answer3, answer3Right);
+        const answer3 = part2(list_of_ints);
+        let answer3Right;
+        if (answers.length > 1) { 
+            answer3Right = answers[1] == answer3.toString();
+        }
+        // hmm 9007199254740991 is too high 
+        console.log('part 2 answer', answer3, answer3Right);
     });
 })
 
