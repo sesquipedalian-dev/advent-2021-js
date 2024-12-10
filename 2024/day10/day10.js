b0cc5de669f990eaf4bb84386201cd8548b97329
The Sesquipedalian Dev <sesquipedalian.dev@gmail.com>
2024-12-09 22:38:02 -0700

    holier shit, second time!

--- 2024/day10/day10.js
+++ 2024/day10/day10.js
@@ -37,8 +37,42 @@ const part1 = (grid) => {
     return sum;
 }
 
-const part2 = () => {
-    return null;
+const part2 = (grid) => {
+    // ok so this time, we can't prune branches that we have already visited? 
+    // whichever way we can reach back to point 0 adds to point 0's tally
+    // does building all the routes along the way help us? 
+
+     // grid.print()
+
+    // keep a tally of 9s accessible at each position
+    // find all the 9s as the starting point
+    // dfs from there to a 0, only taking 1 magnitude steps 
+    // during this DFS only visit each coord once (hold onto a visited collection)
+    grid.iterate((row, column, i) => { 
+        if (!i.blank && i.height == 9) { 
+            const toVisit = [[row, column]]
+            while(toVisit.length > 0) { 
+                const [vRow, vColumn] = toVisit.pop()
+                grid.at(vRow, vColumn).tally += 1
+                grid.neighboringIndexes(vRow, vColumn)
+                    .filter(([nRow, nColumn]) => {
+                        const n = grid.at(nRow, nColumn)
+                        return !n.blank && n.height == (grid.at(vRow, vColumn).height - 1 )
+                    })
+                    .forEach(p => toVisit.push(p))
+            }
+        }
+    })
+
+    // after, find each 0 and count up their tallies 
+    let sum = 0
+    grid.iterate((row, column, {blank, height, tally}) => {
+        if(!blank && height == 0) { 
+            // console.log('Trailhead score', row, column, tally, sum)
+            sum += tally
+        } 
+    })
+    return sum;
 }
 
 const parse = (input) => { 
@@ -78,12 +112,20 @@ aoc.fetchDayCodes('2024', '10').then(codes => {
         return
     }
 
-    // const part2Answer = part2(sample1);
-    // const part2Correct = utils.parseAnswerFromEms(codes[codes.length - 1]);
-    // if (part2Answer != part2Correct) {
-    //     console.log('failed on part 2 test case', part2Answer, part2Correct);
-    //     return;
-    // }
+    const part2Sample2 = parse(codes[30])
+    const part2Real2 = parseInt(codes[31])
+    const part2Answer2 = part2(part2Sample2)
+    if (part2Answer2 != part2Real2) { 
+        console.log('failed on part 2 test case 2', part2Real2, part2Answer2)
+        return
+    }
+
+    const part2Answer = part2(parse(codes[18]));
+    const part2Correct = utils.parseAnswerFromEms(codes[51]);
+    if (part2Answer != part2Correct) {
+        console.log('failed on part 2 test case 1', part2Answer, part2Correct);
+        return;
+    }
 
     Promise.all([aoc.fetchDayInput('2024', '10'), aoc.fetchDayAnswers('2024', '10')]).then(([input, answers]) => {
 
@@ -95,12 +137,12 @@ aoc.fetchDayCodes('2024', '10').then(codes => {
         }
         console.log('part 1 answer', answer2, answer2Right);
 
-        // const answer3 = part2(list_of_ints);
-        // let answer3Right;
-        // if (answers.length > 1) { 
-        //     answer3Right = answers[1] == answer3.toString();
-        // }
-        // console.log('part 2 answer', answer3, answer3Right);
+        const answer3 = part2(parse(input));
+        let answer3Right;
+        if (answers.length > 1) { 
+            answer3Right = answers[1] == answer3.toString();
+        }
+        console.log('part 2 answer', answer3, answer3Right);
     });
 })
 
